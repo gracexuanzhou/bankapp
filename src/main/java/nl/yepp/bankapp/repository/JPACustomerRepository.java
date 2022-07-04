63f4c632e936bed14ac91944043fa33780cffa89
Ruslan Forostianov <ruslan@forostianov.com>
2022-07-04 10:34:35 +0200

    Simplify custom repository by reusing spring CrudRepository

--- src/main/java/nl/yepp/bankapp/repository/JPACustomerRepository.java
+++ src/main/java/nl/yepp/bankapp/repository/JPACustomerRepository.java
@@ -2,6 +2,8 @@ package nl.yepp.bankapp.repository;
 
 import nl.yepp.bankapp.model.Customer;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -12,36 +14,6 @@ import java.util.Optional;
 
 @Service
 @Primary
-@Transactional
-public class JPACustomerRepository implements CustomerRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
-    @Override
-    public Customer save(Customer customer) {
-        Long customerId = customer.getId();
-        if (customerId == null) {
-            entityManager.persist(customer);
-        } else {
-            entityManager.merge(customer);
-        }
-        return customer;
-    }
+public interface JPACustomerRepository extends CrudRepository<Customer, Long>, CustomerRepository {
 
-    @Override
-    public Optional<Customer> findById(Long customerId) {
-        return Optional.ofNullable(entityManager.find(Customer.class, customerId));
-    }
-
-    @Override
-    public void deleteById(Long customerId) {
-        Optional<Customer> customer = findById(customerId);
-        if (!customer.isEmpty()) {
-            entityManager.remove(customer.get());
-        }
-    }
-
-    @Override
-    public List<Customer> findAll() {
-        return entityManager.createQuery("select c from Customer c").getResultList();
-    }
 }
